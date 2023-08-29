2ae037e0932c455d32606b9aab1b4edd61c70318
Lurie <lurainekimmerle@gmail.com>
2023-08-29 07:36:31 +0000

    add email to form

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ export default function App() {
     
     const [formData, setFormData]= React.useState({
       firstName: "",
-      lastName: ""
+      lastName: "",
+      email:""
     })
     console.log(formData)
-    
+  
     
     function handleChange(event){
     
@@ -32,13 +33,19 @@ export default function App() {
                 onChange= {handleChange}
                 name= "firstName"
             />
-
             <input
                 type="text"
                 placeholder="Last Name"
                 onChange= {handleChange}
                 name= "lastName"
             />
+            <input
+                type="email"
+                placeholder="Email"
+                onChange= {handleChange}
+                name= "email"
+            />
+
         </form>
     )
 }
