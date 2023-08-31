06c9183ec1bb717c1beb0cbfac36e044bcd1b676
Lurie <lurainekimmerle@gmail.com>
2023-08-31 02:31:45 +0000

    add styles and isFriendlym employment and favColor to state

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ export default function App() {
       lastName: "",
       email:"",
       comment: "",
-      checked: true
+      isFriendly: true,
+      employment: "",
+      favColor: ""
       
     })
-    console.log(formData)
+   
   
     
     function handleChange(event){
@@ -22,15 +24,18 @@ export default function App() {
           [name]: type === "checkbox" ? checked : value
         }
       })
-
+      
     }
   
     
-   
+   function handleSubmit (){
+      eventthat .preventDefault()
+      console.log(formData)
+   }
     
     
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
 
             <input
                 type="text"
@@ -69,7 +74,59 @@ export default function App() {
               checked={formData.isFriendly}            
             />
             <label htmlFor="isFriendly">Are You Friendly</label>
+            <br/>
+            <br/>
+            <fieldset>
+                <legend>Current employment status </legend>
+                <input 
+                  type="radio"
+                  id = "unemployed"
+                  name="employment"
+                  value="unemployed"
+                  onChange={handleChange}
+                  checked={formData.employment === "unemployed"}
+                  />
+                <label htmlFor="unemployed">Unemployd</label>
+                <br/>
+                <input 
+                  type="radio"
+                  id = "part-time"
+                  name="employment"
+                  value="part-time"
+                  onChange={handleChange}
+                  checked={formData.employment === "part-time"}
+                  />
+                <label htmlFor="part-time">Part-Time</label>
+                <br/>
+                <input 
+                  type="radio"
+                  id = "full-time"
+                  name="employment"
+                  value="full-time"
+                  onChange={handleChange}
+                  checked={formData.employment === "full-time"}
+                  />
+                <label htmlFor="full-time">Full-Time</label>
+                <br/>
+            </fieldset>
+            <br/>
+            <select
+                  id="favColor"
+                  value={formData.favColor}
+                  onChange={handleChange}
+                  name="favColor"
+            >
+                <option value="">--Choose--</option>
+                <option value="red">Red</option>
+                <option value="orange">Orange</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="indigo">Indigo</option>
+                <option value="violet">Violet</option>
 
+            </select>
+            <button >Submit</button>
         </form>
     )
 }
